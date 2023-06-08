dba4f5a2ca87f9e26b2fc96990c71b0c805ddaf3
sp2716 <spalastro@aigcontrols.com>
2023-06-08 08:59:08 -0400

    fixed spacing to adhere to standards of pycharm IDE

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import io
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST', 'DELETE'])
 def user():
     if request.method == 'GET':
@@ -15,16 +16,19 @@ def user():
                        statusCode=200,
                        data=data), 200
 
+
 @app.route('/favicon.ico', methods=['GET'])
 def favicon():
     if request.method == 'GET':
         filename = 'favicon.ico'
         return send_file(filename, mimetype='image/jpg')
 
+
 @app.route('/form', methods=['GET', 'POST', 'DELETE'])
 def form():
-	if request.method == 'GET':
-		return 'form goes here...'
+    if request.method == 'GET':
+        return 'form goes here...'
+
 
 # driver function
 if __name__ == '__main__':
